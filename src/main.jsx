eb7c8779e82fed9549e2a702df271ae32480e94e
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-02-25 17:48:30 +0530

    toCheck

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RecoilRoot } from 'recoil'
+// ankush
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
